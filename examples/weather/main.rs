291aea5a3c0e2c8a2e64b8ff0edf63731b7d89e6
Drake Talley <robertdraketalley@gmail.com>
2020-04-21 22:00:58 -0400

    add run public fn

--- examples/weather/main.rs
+++ examples/weather/main.rs
@@ -1,5 +1,5 @@
 use std::{env, io};
-use vfs_service::{fuse_system, SingleService};
+use vfs_service::{run, SingleService};
 
 mod weather_svc;
 use weather_svc::WeatherService;
@@ -8,19 +8,5 @@ fn main() {
     let weather = Box::new(WeatherService {});
     let svcs: Vec<Box<dyn SingleService + Send>> = vec!(weather);
 
-    unsafe {
-        let fs = fuse_system::Fs::new(svcs);
-
-        let mnt = match env::args().nth(1) {
-            Some(path) => path,
-            None => "./test_dir".to_string(),
-        };
-
-        println!("{}", mnt);
-        let _sys = fuse::spawn_mount(fs, &mnt, &[]).unwrap();
-        let mut str = String::new();
-
-        io::stdin().read_line(&mut str).expect("invalid input");
-        println!("all done!");
-    }
+    run(svcs);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,25 @@ extern crate file_node;
 
 pub use file_node::{SingleService, ServiceDirNode};
 
+pub fn run(svcs: Vec<Box<dyn SingleService + Send>>) {
+    unsafe {
+        let fs = fuse_system::Fs::new(svcs);
+
+        let mnt = match env::args().nth(1) {
+            Some(path) => path,
+            None => "./foo".to_string(),
+        };
+
+        println!("{}", mnt);
+        let _sys = fuse::spawn_mount(fs, &mnt, &[]).unwrap();
+        let mut str = String::new();
+
+        io::stdin().read_line(&mut str).expect("invalid input");
+        println!("all done!");
+    }
+
+}
+
 pub fn init(svc: Vec<Box<dyn SingleService + Send>>) {
     //logger::foo();
 
